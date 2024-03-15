e0b1f0553b652e0918b474e184a30d1e8c13cdfa
annaprianka <122701615+annaprianka@users.noreply.github.com>
2024-03-15 23:30:58 +0100

    Add files via upload

---
+++ client/pick-a-petal/src/components/post/HardcodedPost/PostBluete.jsx
@@ -0,0 +1,23 @@
+import "../post.css"
+import { Link } from "react-router-dom";
+
+export default function PostGoogle() {
+  return (
+    <div className="post">
+      <img className="postImg" src="https://cdn.pixabay.com/photo/2020/05/19/16/38/begonia-5191628_960_720.jpg" alt="Begonienblüten"/>
+      <div className="postInfo">
+         <div className="postCats">
+             <span className="postCat">Care</span>
+             <span className="postCat"></span>
+         </div> 
+         <Link className="link" to ="/HardcodedPost/PostBluete">
+         <span className="postTitle"> Die Blütenpracht im eigenen Wohnzimmer: Ein Leitfaden für blühende Zimmerpflanzen</span> </Link>
+         <hr />
+         <span className="postDate">18.02.2024</span>
+      </div>
+      <p className="postDes"> Zimmerpflanzen verleihen deinem Zuhause etwas Lebendiges.   Etwas Grün im Zimmer bringt Natur in den Innenraum, aber was ist, wenn man dann doch noch ein paar andere Farben mit einbringen will. Blühende Zimmerpflanzen geben dir genau das.
+          In diesem Blogbeitrag entdecken wir nicht nur ihre atemberaubende Schönheit, sondern auch die Geheimnisse ihrer Pflege und Vermehrung. Mach dich bereit, deinem Zuhause eine blühende Atmosphäre zu verleihen, während wir die beliebtesten blühenden Zimmerpflanzen erkunden und uns mit ihren Vermehrungsmethoden sowie der Kunst der Blütenförderung vertraut machen.
+      </p>
+    </div>
+  )
+}
---
+++ client/pick-a-petal/src/components/post/HardcodedPost/PostGoogle.jsx
@@ -0,0 +1,22 @@
+import "../post.css"
+import "../../post/HardcodedPost/PostGoogle"
+
+export default function PostGoogle() {
+  return (
+    <div className="post">
+      <img className="postImg" src="https://cdn.pixabay.com/photo/2020/09/02/03/26/iphone-5537230_1280.jpg" alt="Smartphone in einer Hand"/>
+      <div className="postInfo">
+         <div className="postCats">
+             <span className="postCat">Care</span>
+             <span className="postCat"></span>
+         </div>
+         <Link className="link" to ="/HardcodedPost/PostGoogle">
+         <span className="postTitle"> Google Lens & Co: Wie Apps unseren Pflanzen helfen können.</span> </Link>
+         <hr />
+         <span className="postDate"> 05.03.2024</span>
+      </div>
+      <p className="postDes"> In der Welt der Zimmerpflanzen spielt die Technologie eine immer größere Rolle, insbesondere wenn es darum geht, Pflanzen zu identifizieren und ihre Bedürfnisse zu verstehen. Eine der führenden Anwendungen, die dabei hilft, ist Google Lens. Diese App, die auf künstlicher Intelligenz basiert, ermöglicht es Nutzern, Pflanzen schnell und zuverlässig zu bestimmen.
+      </p>
+    </div>
+  )
+}
